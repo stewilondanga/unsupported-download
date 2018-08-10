0f2a5399d166805780d7e21bedf64d97bc6b19b1
Stewart ilondanga <stilondanga@gmail.com>
2018-08-10 13:49:47 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,26 +13,26 @@ var navigate = (function() {
 
 var detect_OS_set_HTML_class = (function() {
 
-      "use strict";
+  "use strict";
 
-      var html = document.documentElement;
-      var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  var html = document.documentElement;
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-      // Windows Phone must come first because its UA also contains "Android"
+  // Windows Phone must come first because its UA also contains "Android"
 
-      if (/windows phone/i.test(userAgent)) {
-        // html.className += " os_Windows";
-        return "Windows";
-      }
+  if (/windows phone/i.test(userAgent)) {
+    // html.className += " os_Windows";
+    return "Windows";
+  }
 
-      if (/android/i.test(userAgent)) {
-        html.className += " os_Android";
-        return "Android";
-      }
+  if (/android/i.test(userAgent)) {
+    html.className += " os_Android";
+    return "Android";
+  }
 
-      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
 
-      /*if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     html.className += " os_iOS";
     return "iOS";
   }
@@ -47,39 +47,39 @@ var detect_OS_set_HTML_class = (function() {
 /* //Faking-up the OS buttons
    Buttons add .os_iOS or .os_Android to <html> */
 
-      /*const fake_OS_detection_set_HTML_class_from_button = (function() {
+/*const fake_OS_detection_set_HTML_class_from_button = (function() {
 
-        "use strict";
+  "use strict";
 
-        const class_iOS = "os_iOS";
-        const class_Android = "os_Android";
-        const html = document.documentElement;
-        const buttons = document.querySelectorAll("[class*=fake_btn]");
+  const class_iOS = "os_iOS";
+  const class_Android = "os_Android";
+  const html = document.documentElement;
+  const buttons = document.querySelectorAll("[class*=fake_btn]");
 
-        if (!buttons) return false;
+  if (!buttons) return false;
 
-        const resetHTMLClass = () => {
-          html.classList.remove(class_iOS, class_Android);
-        };
+  const resetHTMLClass = () => {
+    html.classList.remove(class_iOS, class_Android);
+  };
 
-        const setHTMLClass = (btn, clss, is_os) => {
-          if (btn.className.match(clss)) {
-            if (!is_os) {
-              html.classList.add(clss);
-            }
-          }
-        };
+  const setHTMLClass = (btn, clss, is_os) => {
+    if (btn.className.match(clss)) {
+      if (!is_os) {
+        html.classList.add(clss);
+      }
+    }
+  };
 
-        const fakeBtnClick = (event) => {
-          const btn = event.target;
-          const is_iOS = html.classList.contains(class_iOS);
-          const is_Android = html.classList.contains(class_Android);
-          resetHTMLClass();
-          setHTMLClass(btn, class_iOS, is_iOS);
-          setHTMLClass(btn, class_Android, is_Android);
-        };
+  const fakeBtnClick = (event) => {
+    const btn = event.target;
+    const is_iOS = html.classList.contains(class_iOS);
+    const is_Android = html.classList.contains(class_Android);
+    resetHTMLClass();
+    setHTMLClass(btn, class_iOS, is_iOS);
+    setHTMLClass(btn, class_Android, is_Android);
+  };
 
-        for (let btn of buttons) {
-          btn.addEventListener("click", fakeBtnClick);
-        }
-      })();
+  for (let btn of buttons) {
+    btn.addEventListener("click", fakeBtnClick);
+  }
+})();
